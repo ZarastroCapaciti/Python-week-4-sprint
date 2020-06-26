3c5eb0313ef0a500c451de7ecaf3d64d08590e85
ZarastroR37 <62378412+ZarastroR37@users.noreply.github.com>
2020-06-26 11:30:46 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,40 +1,44 @@
-# Program make a simple calculator
+# program -> basic calculator
 
-# This function adds two numbers
+import math # for square root and power of
+# standard addition with two numbers
 def add(x, y):
     return x + y
 
-# This function subtracts two numbers
-
-
+# standard subtraction between two numbers
 def subtract(x, y):
     return x - y
 
-# This function multiplies two numbers
-
-
+# standard multiplication between two numbers
 def multiply(x, y):
     return x * y
 
-# This function divides two numbers
-
-
+# standard division between two numbers
 def divide(x, y):
     return x / y
 
+# to the power of x^y
+def power(x, y):
+    return pow(x, y)
+
+# square root of x
+def square(x):
+    return math.sqrt(x)
 
 print("Select a number to to add")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Power of")
+print("6.Square Root")
 
 while True:
     # Take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5/6): ")
 
     # Check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '5'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -49,6 +53,14 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
-        break
+
+        elif choice == '5':
+            print(num1, "**", num2, "=", power(num1, num2))
+    elif choice == '6':
+        num1 = float(input("Enter the number you want the square root of: "))
+        if num1 < 0:
+            print("Please choose a positive number, as negatives can't go through a square root.")
+            break
+        print("√", num1, "=", square(num1))
     else:
         print("Invalid Input")
